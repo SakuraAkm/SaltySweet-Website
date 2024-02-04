4b2589a5a33f345bbb3094b699fb37f4b090402d
Alex Rinaldi <rinaldialex33@gmail.com>
2024-02-04 23:48:47 +0100

    fixed img

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -47,7 +47,7 @@ export default function Navbar(){
                     
                     <Link to="/" >
                         <div id="logo" className=" d-flex align-items-center">
-                            <img src="./src/assets/logo.png" alt="LOGO" />
+                            <img src="https://i.imgur.com/CYDsWl8.png" alt="LOGO" />
                             <h1>SaltySweet</h1>
                         </div>
                     </Link>
